d609e2e2f4b3465ff208153e8a34cac92529c563
dian-anindita <ditaa09@gmail.com>
2020-07-22 13:28:10 +0700

    tugas hari ke-3

---
+++ Pekan 1/Tugas Hari Ke-3/tugas3.js
@@ -0,0 +1,95 @@
+//soal 1
+var kataPertama = "saya";
+var kataKedua = "senang";
+var kataKetiga = "belajar";
+var kataKeempat = "javascript";
+
+kataKedua = kataKedua.charAt(0).toUpperCase() + kataKedua.substr(1);
+var hasil = kataPertama + ' ' + kataKedua + ' ' + kataKetiga + ' ' + kataKeempat.toUpperCase();
+console.log(hasil);
+
+//soal 2
+var kataPertama = "1";
+var kataKedua = "2";
+var kataKetiga = "4";
+var kataKeempat = "5";
+
+var jumlah = parseInt(kataPertama) + parseInt(kataKedua) + parseInt(kataKetiga) + parseInt(kataKeempat);
+console.log(jumlah);
+
+//soal 3
+var kalimat = 'wah javascript itu keren sekali'; 
+
+var kataPertama = kalimat.substring(0, 3); 
+var kataKedua = kalimat.substring(4, 14);
+var kataKetiga = kalimat.substring(15, 18);
+var kataKeempat = kalimat.substring(19, 24);
+var kataKelima = kalimat.substring(25, 31);
+
+console.log('Kata Pertama: ' + kataPertama); 
+console.log('Kata Kedua: ' + kataKedua); 
+console.log('Kata Ketiga: ' + kataKetiga); 
+console.log('Kata Keempat: ' + kataKeempat); 
+console.log('Kata Kelima: ' + kataKelima);
+
+//soal 4
+var nilai = 90;
+
+if ((nilai >= 80)) 
+{
+    console.log("Indeks nilai = A");
+}
+else if ((nilai >=70 ) && (nilai < 80)) 
+{
+    console.log("Indeks nilai = B");
+}
+else if ((nilai >=60 ) && (nilai < 70)) 
+{
+    console.log("Indeks nilai = C");
+}
+else if ((nilai >=50 ) && (Nilai < 60)) 
+{
+    console.log("Indeks nilai = D");
+}
+else if ((nilai < 50)) 
+{
+    console.log("Indeks nilai = E");
+}
+else{
+    console.log("Indeks nilai tidak terpenuhi");
+}
+
+//soal 5
+var tanggal = 9;
+var bulan = 6;
+var tahun = 1998;
+
+switch(bulan) {
+    case 1: bulan = "Januari";
+    break;
+    case 2: bulan = "Februari";
+    break;
+    case 3: bulan = "Maret";
+    break;
+    case 4: bulan = "April";
+    break;
+    case 5: bulan = "Mei";
+    break;
+    case 6: bulan = "Juni";
+    break;
+    case 7: bulan = "Juli";
+    break;
+    case 8: bulan = "Agustus";
+    break;
+    case 9: bulan = "September";
+    break;
+    case 10: bulan = "Oktober";
+    break;
+    case 11: bulan = "November";
+    break;
+    case 12: bulan = "Desember";
+    break;
+   }
+
+   var hasil = tanggal + ' ' + bulan + ' ' + tahun;
+   console.log(hasil);
