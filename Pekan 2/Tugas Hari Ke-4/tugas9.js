2f42ebfa13c70efde050b139b5406229651d5f72
dian-anindita <ditaa09@gmail.com>
2020-07-30 16:47:12 +0700

    update tugas9 (es6 part 2)

---
+++ Pekan 2/Tugas Hari Ke-4/tugas9.js
@@ -0,0 +1,36 @@
+console.log("===============Soal 1===============")
+//soal 1
+const newFunction = function literal(firstName, lastName){
+    return {
+      firstName,
+      lastName,
+      fullName: () =>{
+        console.log(`${firstName} ${lastName}`)
+        return 
+      }
+    }
+  }
+   
+  //Driver Code 
+  newFunction("William", "Imoh").fullName()
+
+console.log("===============Soal 2===============")
+//soal 2
+const newObject = {
+    firstName: "Harry",
+    lastName: "Potter Holt",
+    destination: "Hogwarts React Conf",
+    occupation: "Deve-wizard Avocado",
+    spell: "Vimulus Renderus!!!"
+  }
+
+  const {firstName, lastName, destination, occupation, spell} = newObject
+  console.log(firstName, lastName, destination, occupation)
+
+console.log("===============Soal 3===============")
+//soal 3
+const west = ["Will", "Chris", "Sam", "Holly"]
+const east = ["Gill", "Brian", "Noel", "Maggie"]
+const combined = [...west, ...east]
+//Driver Code
+console.log(combined)
